3ac568f0cb1284a6b271f245bc6d8a83c562743c
Damir Sarkulin <sarkulin@inbox.ru>
2018-02-05 13:15:29 +0500

    Итерация по массивам

--- index.php
+++ index.php
@@ -3,6 +3,48 @@ $is_auth = (bool) rand(0, 1);
 
 $user_name = 'Константин';
 $user_avatar = 'img/user.jpg';
+
+$categories = ['Доски и лыжи', 'Крепления', 'Ботинки', 'Одежда', 'Инструменты', 'Разное'];
+$ads_list = [
+    0 => [
+        'name' => '2014 Rossignol District Snowboard',
+        'category' => 'Доски и лыжи',
+        'price' => '10999',
+        'img_url' => 'img/lot-1.jpg'
+    ],
+    1 => [
+        'name' => 'DC Ply Mens 2016/2017 Snowboard',
+        'category' => 'Доски и лыжи',
+        'price' => '159999',
+        'img_url' => 'img/lot-2.jpg'
+    ],
+    2 => [
+        'name' => 'Крепления Union Contact Pro 2015 года размер L/XL',
+        'category' => 'Крепления',
+        'price' => '8000',
+        'img_url' => 'img/lot-3.jpg'
+    ],
+    3 => [
+        'name' => 'Ботинки для сноуборда DC Mutiny Charocal',
+        'category' => 'Ботинки',
+        'price' => '10999',
+        'img_url' => 'img/lot-4.jpg'
+    ],
+    4 => [
+        'name' => 'Куртка для сноуборда DC Mutiny Charocal',
+        'category' => 'Одежда',
+        'price' => '7500',
+        'img_url' => 'img/lot-5.jpg'
+    ],
+    5 => [
+        'name' => 'Маска Oakley Canopy',
+        'category' => 'Разное',
+        'price' => '5400',
+        'img_url' => 'img/lot-6.jpg'
+    ]
+    ];
+$index = 0;
+$num_count = count($categories);
 ?>
 <!DOCTYPE html>
 <html lang="ru">
@@ -81,17 +123,18 @@ $user_avatar = 'img/user.jpg';
             <h2>Открытые лоты</h2>
         </div>
         <ul class="lots__list">
+            <?php foreach ($ads_list as $key => $item): ?>
             <li class="lots__item lot">
                 <div class="lot__image">
-                    <img src="img/lot-1.jpg" width="350" height="260" alt="Сноуборд">
+                    <img src="<?=$item['img_url'];?>" width="350" height="260" alt="">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category">Доски и лыжи</span>
-                    <h3 class="lot__title"><a class="text-link" href="lot.html">2014 Rossignol District Snowboard</a></h3>
+                    <span><?=$item['name'];?></span>
+                    <h3><?=$item['category']; ?><a class="text-link" href="lot.html"></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost">10 999<b class="rub">р</b></span>
+                            <span><?=$item['price'];?><b class="rub">р</b></span>
                         </div>
                         <div class="lot__timer timer">
 
@@ -99,6 +142,7 @@ $user_avatar = 'img/user.jpg';
                     </div>
                 </div>
             </li>
+            <?php endforeach; ?>
         </ul>
     </section>
 </main>
@@ -106,24 +150,10 @@ $user_avatar = 'img/user.jpg';
 <footer class="main-footer">
     <nav class="nav">
         <ul class="nav__list container">
-            <li class="nav__item">
-                <a href="all-lots.html">Доски и лыжи</a>
-            </li>
-            <li class="nav__item">
-                <a href="all-lots.html">Крепления</a>
-            </li>
-            <li class="nav__item">
-                <a href="all-lots.html">Ботинки</a>
-            </li>
-            <li class="nav__item">
-                <a href="all-lots.html">Одежда</a>
-            </li>
-            <li class="nav__item">
-                <a href="all-lots.html">Инструменты</a>
-            </li>
-            <li class="nav__item">
-                <a href="all-lots.html">Разное</a>
-            </li>
+        <?php while($index < $num_count): ?>
+        <a href="/?cat=<?=$index;?>"><?=$categories[$index];?></a>
+        <?php $index = $index +1; ?>
+        <?php endwhile; ?>
         </ul>
     </nav>
     <div class="main-footer__bottom container">
