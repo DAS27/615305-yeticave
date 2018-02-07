91aa34aecc0c4ad0a3647b1912f5cb37305bc4f9
Damir Sarkulin <sarkulin@inbox.ru>
2018-02-07 10:16:55 +0500

    Вношу изменения

--- index.php
+++ index.php
@@ -9,37 +9,37 @@ $ads_list = [
     [
         'name' => '2014 Rossignol District Snowboard',
         'category' => 'Доски и лыжи',
-        'price' => '10999',
+        'price' => 10999,
         'img_url' => 'img/lot-1.jpg'
     ],
     [
         'name' => 'DC Ply Mens 2016/2017 Snowboard',
         'category' => 'Доски и лыжи',
-        'price' => '159999',
+        'price' => 159999,
         'img_url' => 'img/lot-2.jpg'
     ],
     [
         'name' => 'Крепления Union Contact Pro 2015 года размер L/XL',
         'category' => 'Крепления',
-        'price' => '8000',
+        'price' => 8000,
         'img_url' => 'img/lot-3.jpg'
     ],
     [
         'name' => 'Ботинки для сноуборда DC Mutiny Charocal',
         'category' => 'Ботинки',
-        'price' => '10999',
+        'price' => 10999,
         'img_url' => 'img/lot-4.jpg'
     ],
     [
         'name' => 'Куртка для сноуборда DC Mutiny Charocal',
         'category' => 'Одежда',
-        'price' => '7500',
+        'price' => 7500,
         'img_url' => 'img/lot-5.jpg'
     ],
     [
         'name' => 'Маска Oakley Canopy',
         'category' => 'Разное',
-        'price' => '5400',
+        'price' => 5400,
         'img_url' => 'img/lot-6.jpg'
     ]
     ];
@@ -147,9 +147,9 @@ $ads_list = [
 <footer class="main-footer">
     <nav class="nav">
         <ul class="nav__list container nav__item">
-                <?php foreach ($categories as $key): ?>
+                <?php foreach ($categories as $category_item): ?>
             <li class="nav__item">
-                <a href="all-lost.html"><?php print($key); ?></a>
+                <a href="all-lost.html"><?php print($category_item); ?></a>
             </li>
                 <?php endforeach; ?>
         </ul>
