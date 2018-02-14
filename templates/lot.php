d2e26aad5d5399cc9410ed4ed66273c6a6898668
Damir Sarkulin <sarkulin@inbox.ru>
2018-02-14 13:16:13 +0500

    Add lots templates

--- templates/lot.php
+++ templates/lot.php
@@ -26,7 +26,7 @@
           <div class="lot-item__cost-state">
             <div class="lot-item__rate">
               <span class="lot-item__amount">Текущая цена</span>
-              <span class="lot-item__cost"><?=htmlspecialchars($lot['price']); ?></span>
+              <span class="lot-item__cost"><?=format_price($lot['price']); ?></span>
             </div>
             <div class="lot-item__min-cost">
               Мин. ставка <span>12 000 р</span>
