73b322388967a1ca6798636c99dbf6c8bab8a8c0
Damir Sarkulin <sarkulin@inbox.ru>
2018-02-14 13:11:39 +0500

    Работа со строкой запроса

--- lot.php
+++ lot.php
@@ -6,24 +6,20 @@ $lot = null;
 
 if (isset($_GET['id'])) {
     $id = $_GET['id'];
-    foreach ($lots as $item => $key) {
+    foreach ($lots as $key => $item) {
         if ($key == $id) {
+        
             $lot = $item;
         break;
         }
     }
 }
 
-if (null) {
+if ($lot === null) {
 	http_response_code(404);
 }
 
-$page_content = render_template('lot', 
-['name' => $lots,
- 'category' => $lots,
- 'price' => $lots,
- 'image' => $lots
-]);
+$page_content = render_template('lot', ['lot' => $lot]);
 
 $layout_content = render_template('layout',
 ['content' => $page_content,
