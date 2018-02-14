309982fc3a8c95032a77f4d9a37fdfa5bcaebf4b
Damir Sarkulin <sarkulin@inbox.ru>
2018-02-14 09:22:22 +0500

    Fixed

--- templates/index.php
+++ templates/index.php
@@ -28,18 +28,18 @@
             <h2>Открытые лоты</h2>
         </div>
         <ul class="lots__list">
-            <?php foreach ($lots_list as $item): ?>
+            <?php foreach ($lots as $item => $key): ?>
                 <li class="lots__item lot">
                     <div class="lot__image">
-                        <img src="<?=$item['img_url'];?>" width="350" height="260" alt="">
+                        <img src="<?=$key['image'];?>" width="350" height="260" alt="">
                     </div>
                 <div class="lot__info">
-                    <span><?=$item['category'];?></span>
-                        <h3><a class="text-link" href="lot.html"><?=$item['name']; ?></a></h3>
+                    <span><?=$key['category'];?></span>
+                        <h3><a class="text-link" href="lot.php?id=<?=$item ?>"><?=$key['name'];?></a></h3>
                             <div class="lot__state">
                                 <div class="lot__rate">
                                     <span class="lot__amount">Стартовая цена</span>
-                                    <span class="lot__cost"><?=format_price($item['price']);?></span>
+                                    <span class="lot__cost"><?=format_price($key['price']);?></span>
                                 </div>
                             <div class="lot__timer timer">
                             <?=get_lot_time();?>
