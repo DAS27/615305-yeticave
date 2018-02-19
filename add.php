2525f75596e1f7d682a4573a186dfe4e1c245b74
Damir Sarkulin <sarkulin@inbox.ru>
2018-02-19 10:31:04 +0500

    add lot

---
+++ add.php
@@ -0,0 +1,38 @@
+<?php
+require_once 'functions.php';
+require_once 'data.php';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $lot = $_POST;
+
+    $required = ['lot-name', 'message'];
+    $dict = ['lot-name' => 'Наименование', 'message' => 'Описание'];
+    $errors = [];
+
+    foreach ($required as $key) {
+        if (empty($_POST[$key])) {
+            $errors[$key] = 'Это поле объязательно для заполнения!!!';
+        }
+    }
+
+    if (count($errors)) {
+        $page_content = render_template('add', ['lot' => $lot, 'errors' => $errors, 'dict' => $dict]);
+    }
+    else {
+        $page_content = render_template('lot', ['lot' => $lot]);
+    }
+}
+else {
+    $page_content = render_template('add', []);
+}
+
+$layout_content = render_template('layout',
+['content' => $page_content,
+ 'title' => 'YetiCave - Интернет-аукцион для сноубордического и горнолыжного снаряжения',
+ 'categories' => $categories,
+ 'is_auth' => $is_auth,
+ 'user_name' => $user_name,
+ 'user_avatar' => $user_avatar
+]);
+
+print ($layout_content);
