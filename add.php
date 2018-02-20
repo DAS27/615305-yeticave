8c917890effcab5253011a7a423a9f094a04b1b6
Damir Sarkulin <sarkulin@inbox.ru>
2018-02-20 13:03:48 +0500

    Fixed

--- add.php
+++ add.php
@@ -5,20 +5,69 @@ require_once 'data.php';
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $lot = $_POST;
 
-    $required = ['lot-name', 'message'];
-    $dict = ['lot-name' => 'Наименование', 'message' => 'Описание'];
-    $errors = [];
+$required = ['lot-name', 'category', 'message', 'lot_img', 'lot-rate', 'lot-step', 'lot-date'];
+$dict = [
+    'lot-name' => 'Наименование',
+    'category' => 'Категория',
+    'message' => 'Описание',
+    'lot_img' => 'Изображение',
+    'lot-rate' => 'Начальная цена',
+    'lot-step' => 'Шаг ставки',
+    'lot-date' => 'Дата окончания торгов'
+];
+$errors = [];
 
-    foreach ($required as $key) {
-        if(empty($_POST[$key])) {
-            $errors[$key] = 'Это поле надо заполнить!';
-        }
+foreach ($required as $key) {
+    if(empty($_POST[$key])) {
+        $errors[$key] = 'Это поле надо заполнить!';
     }
-    if (count($errors)) {
-        $page_content = render_template('add', ['lot' => $lot, 'errors' => $errors, 'dict' => $dict]);
+}
+
+if ($value === 'lot-name') {
+     $value = trim($value);
+       if (empty($value)) {
+         $errors [] = "Уберите пробелы";
+   }
+}
+
+if ((!is_int($lot['lot-rate']) && ($lot['lot-rate'] <= 0))) {
+    $errors['lot-rate'] = 'Укажите корректное значение начальной цены в рублях!';
+}
+
+if ((!is_int($lot['lot-step']) && ($lot['lot-step'] <=0 ))) {
+    $errors['lot-step'] = 'Укажите корректное значение шага ставки в рублях!';
+}
+
+if ((strtotime($lot['lot-date']) - strtotime('now')) < 86400) {
+    $errors['lot-date'] = 'Укажите корректное число даты! Дата должна быть больше текущей даты хотя бы на один день!';
+}
+
+if (!empty($_FILES['userfile']['name'])) {
+    $tmp_name = $_FILES['userfile']['tmp_name'];
+    $path = $_FILES['userfile']['name'];
+
+    $finfo = finfo_open(FILEINFO_MIME_TYPE);
+    $file_type = finfo_file($finfo, $tmp_name);
+
+    if ($file_type !== "image/jpeg" && $file_type !== "image/png") {
+    $errors['lot_img'] = 'Загрузите изображение лота в формате jpg или png';
     }
+
     else {
-        $page_content = render_template('lot', ['lot' => $lot]);
+    move_uploaded_file($tmp_name, 'img/' . $path);
+    $lot['path'] = $path;
+    }
+}
+
+else {
+    $errors['lot_img'] = 'Вы не загрузили изображение';
+}
+
+if (count($errors)) {
+    $page_content = render_template('add', ['lot' => $lot, 'errors' => $errors, 'dict' => $dict]);
+}
+else {
+    $page_content = render_template('lot', ['lot' => $lot]);
     }
 }
 else {
@@ -27,7 +76,7 @@ else {
 
 $layout_content = render_template('layout',
 ['content' => $page_content,
- 'title' => 'YetiCave - Интернет-аукцион для сноубордического и горнолыжного снаряжения',
+ 'title' => 'YetiCave - Добавление лота',
  'categories' => $categories,
  'is_auth' => $is_auth,
  'user_name' => $user_name,
