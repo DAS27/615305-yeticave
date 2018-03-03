24d3696bd7264c67910fa9d65bc510dc833e07c4
Damir Sarkulin <sarkulin@inbox.ru>
2018-03-03 09:07:39 +0500

    Added cookies

---
+++ history.php
@@ -0,0 +1,24 @@
+<?php
+require_once('functions.php');
+require_once ('data.php');
+
+$history_lot = [];
+
+if (isset($_COOKIE[$cookie_name_id_lot])){
+    $history_lots_id = json_decode($_COOKIE[$cookie_name_id_lot]);
+    foreach ($history_lots_id as $key => $value){
+        array_push ($history_lot, $lots[$value]);
+    }
+};
+
+$page_content = render_template('history', ['history_lot' => $history_lot ,
+    'bets' => $bets]);
+$layout_content = render_template('layout', [
+    'page_title' => 'История просмотров',
+    'user_avatar' => $user_avatar,
+    'user_name' => $user_name,
+    'is_auth' => $is_auth,
+    'content' => $page_content,
+    'categories' => $categories
+]);
+print ($layout_content);
